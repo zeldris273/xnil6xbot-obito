284636037ba57cf4110f362360d3df17540e3300
zeldris273 <113657396+zeldris273@users.noreply.github.com>
2025-07-22 13:59:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -36,3 +36,13 @@ function startProject() {
 }
 
 startProject();
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Bot is running!');
+});
+
+app.listen(3000, () => {
+  console.log('Uptime server running on port 3000');
+});
